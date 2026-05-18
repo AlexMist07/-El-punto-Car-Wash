939b4db0ea1580646dfacb95cd70ee2c84d2b515
AlexMist07 <mafuentesc07@gmail.com>
2026-05-17 18:14:23 -0600

    fix: actualizar credenciales Supabase

--- carwash/js/supabaseClient.js
+++ carwash/js/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const SUPABASE_URL = 'https://zyeeowpkcpkqjfbloaha.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_YOVPEaAWowYsI_ZOIUPjFQ_iG0wgEvj';
+const SUPABASE_URL = 'https://xeezefqbwddmlunwnhfe.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_25c4a68kG1_4ZpDc4RYvKw_PvfmAqBW';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
